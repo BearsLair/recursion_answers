be1bca01cdee9ee2f0672df0fe0cde4a1f605aaa
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-08-03 23:19:46 -0700

    makes sense now

--- Multi_Dim_Array.js
+++ Multi_Dim_Array.js
@@ -1,17 +1,25 @@
-var seven = totalIntegers([[[5], 3], 0, 2, ["foo"], [], [4, [5, 6]]]); // 7
-console.log(seven);
-
-function totalIntegers(array) {
-  if (array.length === 0) return 0;
-
+function totalIntegers(arr) {
+  console.log("Array/Number being assessed: ", arr);
+  if (arr.length == 0) {
+    console.log("Empty array!");
+    return 0;
+  }
   let total = 0;
-  let first = array.shift();
 
-  if (Array.isArray(first)) {
-    total += totalIntegers(first);
-  } else if (Number.isInteger(first)) {
+  // This line changes the original arr.
+  // And the changed arr is used in the call in the return statment,
+  // facilitating the recursion loop.
+  let shift = arr.shift();
+
+  if (Array.isArray(shift)) {
+    console.log("This is an array, needs breaking down.");
+    total += totalIntegers(shift);
+  } else if (Number.isInteger(shift)) {
+    console.log("Is a number, add 1 to the total.");
     total += 1;
   }
 
-  return total + totalIntegers(array);
+  return total + totalIntegers(arr);
 }
+
+console.log(totalIntegers([[[5], 3], 0, 2, ["foo"], [], [4, [5, 6]]])); // 7
