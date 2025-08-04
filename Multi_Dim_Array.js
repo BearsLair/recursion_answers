101d747dae058db2ac43bef6d8f7c84706d45df3
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-08-03 22:46:02 -0700

    complete, i guess

--- Multi_Dim_Array.js
+++ Multi_Dim_Array.js
@@ -1,24 +1,17 @@
-function totalIntegers(arr) {
-  console.log(arr);
-  if (Array.isArray(arr) && arr.length == 0) {
-    return 0;
-  } else if (Number.isInteger(arr)) {
-    return 1;
-  } else if (Number.isInteger(arr) == false) {
-    return 0;
-  }
+var seven = totalIntegers([[[5], 3], 0, 2, ["foo"], [], [4, [5, 6]]]); // 7
+console.log(seven);
+
+function totalIntegers(array) {
+  if (array.length === 0) return 0;
 
   let total = 0;
-  let shift = arr.shift();
-  console.log(shift);
+  let first = array.shift();
 
-  if (Array.isArray(shift)) {
-    total + totalIntegers(shift);
+  if (Array.isArray(first)) {
+    total += totalIntegers(first);
+  } else if (Number.isInteger(first)) {
+    total += 1;
   }
 
-  return total;
+  return total + totalIntegers(array);
 }
-
-console.log(
-  totalIntegers(totalIntegers([[[5], 3], 0, 2, ["foo"], [], [4, [5, 6]]]))
-); // 7
