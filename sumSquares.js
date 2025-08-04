a699442815056b4200c59799d61690e814202c08
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-08-03 23:21:24 -0700

    initial commit

---
+++ sumSquares.js
@@ -0,0 +1,13 @@
+var l = [1, 2, 3];
+console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+
+l = [[1, 2], 3];
+console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+
+l = [[[[[[[[[1]]]]]]]]];
+console.log(SumSquares(l)); // 1 = 1
+
+l = [10, [[10], 10], [10]];
+console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
+
+function SumSquares(array) {}
