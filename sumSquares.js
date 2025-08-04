0fc9d5f10c22a428e1872f6066365bb03c42131e
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-08-04 00:29:57 -0700

    Need to establish base case

--- sumSquares.js
+++ sumSquares.js
@@ -10,4 +10,20 @@ console.log(SumSquares(l)); // 1 = 1
 l = [10, [[10], 10], [10]];
 console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
 
-function SumSquares(array) {}
+function SumSquares(element) {
+  // Base Case
+  if (element.length == 0) {
+    return 0;
+  }
+
+  let total = 0;
+  let first = element[0];
+
+  if (Array.isArray(first)) {
+    total += SumSquares(first);
+  } else if (Number.isInteger(element)) {
+    return element ** 2;
+  }
+
+  return total + SumSquares(element);
+}
