8abd6f5418c7e62021b03cd92d4f1bc5b939ac14
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-08-04 17:48:18 -0700

    reached solution

--- sumSquares.js
+++ sumSquares.js
@@ -11,18 +11,22 @@ l = [10, [[10], 10], [10]];
 console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
 
 function SumSquares(element) {
+  console.log(element);
   // Base Case
   if (element.length == 0) {
     return 0;
   }
 
   let total = 0;
-  let first = element[0];
+  let first = element.shift();
+  console.log("first: ", first);
 
   if (Array.isArray(first)) {
+    console.log("Is array.");
     total += SumSquares(first);
-  } else if (Number.isInteger(element)) {
-    return element ** 2;
+  } else if (Number.isInteger(first)) {
+    console.log("Is integer.");
+    total += first ** 2;
   }
 
   return total + SumSquares(element);
