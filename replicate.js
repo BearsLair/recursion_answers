72c26b978366857e5e3bddffc7c969e60b58a9cf
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-08-07 22:50:04 -0700

    initial commit

---
+++ replicate.js
@@ -0,0 +1,17 @@
+console.log(replicate(3, 5)); // [5, 5, 5]
+console.log(replicate(1, 69)); // [69]
+console.log(replicate(-2, 6)); // []
+
+function replicate(times, number) {
+  if (times < 0) {
+    return [];
+  }
+
+  // Base case
+  if (times == 0) {
+    return 1;
+  }
+
+  if (times > 0) {
+  }
+}
